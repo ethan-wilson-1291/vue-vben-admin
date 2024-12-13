852c0bce5412dafb173312cc7285dd032d25d2b5
Long Ho <long.ho@creditorwatch.com.au>
2024-12-14 00:56:47 +0700

    feat: Add external auth routes

---
+++ apps/web-np/src/views/_core/authentication/auth-shopify.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { onBeforeMount } from 'vue';
+
+import { Fallback } from '@vben/common-ui';
+import { isShopifyEmbedded } from '@vben/utils';
+
+defineOptions({ name: 'Fallback404Demo' });
+
+onBeforeMount(() => {
+  const a = isShopifyEmbedded();
+  console.log(a);
+});
+</script>
+
+<template>
+  <Fallback status="404" />
+</template>
