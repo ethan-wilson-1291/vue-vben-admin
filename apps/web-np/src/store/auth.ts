929f3a68cbe07ad92366bea226fa15e117005655
Ethan Wilson <ethanwilson1291@gmail.com>
2024-12-14 22:31:45 +0700

    feat: Update auth flow

--- apps/web-np/src/store/auth.ts
+++ apps/web-np/src/store/auth.ts
@@ -86,6 +86,55 @@ export const useAuthStore = defineStore('auth', () => {
     };
   }
 
+  async function authLoginViaToken(
+    accessToken: string,
+    onSuccess?: () => Promise<void> | void,
+  ) {
+    let userInfo: null | UserInfo = null;
+
+    try {
+      loginLoading.value = true;
+
+      // 如果成功获取到 accessToken
+      if (accessToken) {
+        accessStore.setAccessToken(accessToken);
+
+        // 获取用户信息并存储到 accessStore 中
+        const [fetchUserInfoResult, accessCodes] = await Promise.all([
+          fetchUserInfo(),
+          getAccessCodesApi(),
+        ]);
+
+        userInfo = fetchUserInfoResult;
+
+        userStore.setUserInfo(userInfo);
+        accessStore.setAccessCodes(accessCodes);
+
+        if (accessStore.loginExpired) {
+          accessStore.setLoginExpired(false);
+        } else {
+          onSuccess
+            ? await onSuccess?.()
+            : await router.push(userInfo.homePath || DEFAULT_HOME_PATH);
+        }
+
+        if (userInfo?.realName) {
+          notification.success({
+            description: `${$t('authentication.loginSuccessDesc')}:${userInfo?.realName}`,
+            duration: 3,
+            message: $t('authentication.loginSuccess'),
+          });
+        }
+      }
+    } finally {
+      loginLoading.value = false;
+    }
+
+    return {
+      userInfo,
+    };
+  }
+
   async function logout(redirect: boolean = true) {
     try {
       await logoutApi();
@@ -121,6 +170,7 @@ export const useAuthStore = defineStore('auth', () => {
     $reset,
     authLogin,
     authLoginViaShopifySession,
+    authLoginViaToken,
     fetchUserInfo,
     loginLoading,
     logout,
