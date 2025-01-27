14c47e54556f12b52f6dfbd8f4d0596caec69274
Ethan Wilson <ethanwilson1291@gmail.com>
2025-01-27 12:43:29 +0700

    feat: Update layout

--- apps/web-np/src/views/reports/order/index.vue
+++ apps/web-np/src/views/reports/order/index.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
+import type { VbenFormProps } from '#/adapter/form';
 import type { VxeGridProps } from '#/adapter/vxe-table';
 
 import { Page } from '@vben/common-ui';
 
 import { Button } from 'ant-design-vue';
+import dayjs from 'dayjs';
 
 import { useVbenVxeGrid } from '#/adapter/vxe-table';
 import { calcPercentage, getReportOrderApi } from '#/api';
@@ -17,87 +19,180 @@ interface RowType {
   releaseDate: string;
 }
 
+const formOptions: VbenFormProps = {
+  // 默认展开
+  collapsed: false,
+  fieldMappingTime: [['date', ['start', 'end']]],
+  schema: [
+    {
+      component: 'Input',
+      defaultValue: '1',
+      fieldName: 'category',
+      label: 'Category',
+    },
+    {
+      component: 'Input',
+      fieldName: 'productName',
+      label: 'ProductName',
+    },
+    {
+      component: 'Input',
+      fieldName: 'price',
+      label: 'Price',
+    },
+    {
+      component: 'Select',
+      componentProps: {
+        allowClear: true,
+        options: [
+          {
+            label: 'Color1',
+            value: '1',
+          },
+          {
+            label: 'Color2',
+            value: '2',
+          },
+        ],
+        placeholder: '请选择',
+      },
+      fieldName: 'color',
+      label: 'Color',
+    },
+    {
+      component: 'RangePicker',
+      defaultValue: [dayjs().subtract(7, 'days'), dayjs()],
+      fieldName: 'date',
+      label: 'Date',
+    },
+  ],
+  // 控制表单是否显示折叠按钮
+  showCollapseButton: true,
+  // 是否在字段值改变时提交表单
+  submitOnChange: true,
+  // 按下回车时是否提交表单
+  submitOnEnter: false,
+};
+
 const gridOptions: VxeGridProps<RowType> = {
   checkboxConfig: {
     highlight: true,
-    labelField: 'name',
+    labelField: 'id',
   },
   columns: [
     {
       field: 'name',
-      title: 'Name',
-      type: 'checkbox',
-      align: 'right',
+      title: 'Order ID',
+      fixed: 'left',
+      minWidth: 120,
+    },
+    {
+      field: 'processedAt',
+      title: 'Date',
+      formatter: 'npFormatDateTime',
+      minWidth: 110,
     },
     {
       cellRender: { name: 'CellNumber', props: { type: 'success' } },
       field: 'grossSales',
       title: 'Gross Sales',
       align: 'right',
+      minWidth: 150,
     },
     {
       cellRender: { name: 'CellNumber', props: { type: 'success' } },
       field: 'customerFee',
-      title: 'Customer Paid Fees',
+      title: 'Customer Paid',
+      titleHelp: {
+        content: 'The fees paid by the customer. Ex: shipping, tips, etc.',
+      },
       align: 'right',
+      minWidth: 140,
     },
     {
       cellRender: { name: 'CellNumber', props: { type: 'danger' } },
       field: 'discount',
       title: 'Discount',
       align: 'right',
+      minWidth: 120,
     },
     {
       cellRender: { name: 'CellNumber', props: { type: 'danger' } },
       field: 'refundTotal',
-      title: 'Total Refund',
+      title: 'Refund',
       align: 'right',
+      minWidth: 120,
     },
     {
       cellRender: { name: 'CellNumber', props: { strong: true } },
       field: 'netPayment',
-      title: 'Net Payment',
+      title: 'Revenue',
+      titleHelp: {
+        content:
+          'Net payment = Revenue = Gross sales + Customer paid - Discount - Refund',
+      },
       align: 'right',
+      minWidth: 150,
     },
     {
       cellRender: { name: 'CellNumber', props: { type: 'danger' } },
       field: 'shippingCosts',
       title: 'Shipping Cost',
       align: 'right',
+      minWidth: 120,
     },
     {
       cellRender: { name: 'CellNumber', props: { type: 'danger' } },
       field: 'cogs',
       title: 'COGS',
+      titleHelp: {
+        content:
+          'Cost of Goods Sold (COGS) is the direct costs attributable to the production of the goods sold in a company. This amount includes the cost of the materials used in creating the good along with the direct labor costs used to produce the good.',
+      },
       align: 'right',
+      minWidth: 150,
     },
     {
       cellRender: { name: 'CellNumber', props: { type: 'danger' } },
       field: 'handlingFees',
-      title: 'Handling Fees',
+      title: 'Handling',
+      titleHelp: {
+        content:
+          'Handling fees are the costs associated with the handling of goods, including the cost of labor, packaging, and shipping.',
+      },
       align: 'right',
+      minWidth: 120,
     },
     {
       cellRender: { name: 'CellNumber', props: { type: 'danger' } },
       field: 'transactionFees',
-      title: 'Transaction Fees',
+      title: 'Transaction',
+      titleHelp: {
+        content:
+          'Transaction fees are fees that a merchant must pay every time a customer makes a purchase with a credit card. These fees can vary depending on the credit card company and the merchant account provider.',
+      },
       align: 'right',
+      minWidth: 120,
     },
     {
       cellRender: { name: 'CellNumber', props: { strong: true } },
       field: 'grossProfit',
       title: 'Gross Profit',
+      titleHelp: {
+        content:
+          'Gross profit = Revenue - COGS - Shipping cost - Handling - Transaction',
+      },
       align: 'right',
+      minWidth: 150,
     },
     {
       slots: { default: 'gross-profit-margin' },
       title: 'Gross Profit Margin',
+      titleHelp: {
+        content: 'Gross profit margin = (Gross profit / Revenue) * 100%',
+      },
       align: 'right',
-    },
-    {
-      field: 'processedAt',
-      title: 'Date',
-      formatter: 'formatDateTime',
+      minWidth: 170,
     },
   ],
   exportConfig: {},
@@ -114,13 +209,14 @@ const gridOptions: VxeGridProps<RowType> = {
     },
   },
   toolbarConfig: {
+    search: true,
     custom: true,
     refresh: true,
     zoom: true,
   },
 };
 
-const [Grid, gridApi] = useVbenVxeGrid({ gridOptions });
+const [Grid, gridApi] = useVbenVxeGrid({ gridOptions, formOptions });
 </script>
 
 <template>
