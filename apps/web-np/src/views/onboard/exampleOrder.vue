c9af3272d134196e4d2ca6f13f694f764a864d16
Ethan Wilson <ethanwilson1291@gmail.com>
2025-02-07 00:34:46 +0700

    feat: onboard - update transaction setting

--- apps/web-np/src/views/onboard/exampleOrder.vue
+++ apps/web-np/src/views/onboard/exampleOrder.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { computed } from 'vue';
+
 import { Card } from 'ant-design-vue';
 
 import { useShopStore } from '#/store';
@@ -7,44 +9,63 @@ import { formatMoney } from '#/utils';
 import { sampleOrder } from './service';
 
 const shopStore = useShopStore();
-const currencySymbol = shopStore.shop.currencySymbol;
+const totalCost = computed(() => {
+  return (
+    sampleOrder.totalCogs +
+    sampleOrder.totalHandlingFees +
+    sampleOrder.shippingFees +
+    sampleOrder.transactionFees
+  );
+});
 </script>
 
 <template>
   <Card title="Example Order" class="min-w-56">
     <div class="mb-2 flex justify-between font-bold">
       <div>Net Payment</div>
-      <div>{{ formatMoney(sampleOrder.grossSales, currencySymbol) }}</div>
-    </div>
-    <div class="flex justify-between">
-      <div>Total COGS</div>
-      <div>{{ formatMoney(sampleOrder.totalCogs, currencySymbol) }}</div>
-    </div>
-    <div class="flex justify-between">
-      <div>Handling Fees</div>
       <div>
-        {{ formatMoney(sampleOrder.totalHandlingFees, currencySymbol) }}
+        {{ formatMoney(sampleOrder.grossSales, shopStore.shop.currency) }}
       </div>
     </div>
-    <div class="flex justify-between">
-      <div>Shipping Costs</div>
-      <div>{{ formatMoney(sampleOrder.shippingFees, currencySymbol) }}</div>
+    <div class="text-md flex justify-between font-bold">
+      <div>Total costs</div>
+      <div>
+        {{ formatMoney(totalCost, shopStore.shop.currency) }}
+      </div>
     </div>
-    <div class="flex justify-between">
-      <div>Transaction Fees</div>
-      <div>{{ formatMoney(sampleOrder.transactionFees, currencySymbol) }}</div>
+    <div class="flex justify-between text-sm">
+      <div>- Total COGS</div>
+      <div>
+        {{ formatMoney(sampleOrder.totalCogs, shopStore.shop.currency) }}
+      </div>
+    </div>
+    <div class="flex justify-between text-sm">
+      <div>- Handling Fees</div>
+      <div>
+        {{
+          formatMoney(sampleOrder.totalHandlingFees, shopStore.shop.currency)
+        }}
+      </div>
+    </div>
+    <div class="flex justify-between text-sm">
+      <div>- Shipping Costs</div>
+      <div>
+        {{ formatMoney(sampleOrder.shippingFees, shopStore.shop.currency) }}
+      </div>
+    </div>
+    <div class="flex justify-between text-sm">
+      <div>- Transaction Fees</div>
+      <div>
+        {{ formatMoney(sampleOrder.transactionFees, shopStore.shop.currency) }}
+      </div>
     </div>
     <div class="text-md mt-2 flex justify-between font-bold">
-      <div>Profit</div>
+      <div>Gross Profit</div>
       <div>
         {{
           formatMoney(
-            sampleOrder.grossSales -
-              sampleOrder.totalCogs -
-              sampleOrder.totalHandlingFees -
-              sampleOrder.shippingFees -
-              sampleOrder.transactionFees,
-            currencySymbol,
+            sampleOrder.grossSales - totalCost,
+            shopStore.shop.currency,
           )
         }}
       </div>
