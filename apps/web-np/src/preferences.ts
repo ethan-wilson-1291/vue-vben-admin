86191fe4a2dfc5df644441b424c116095ac619ae
Ethan Wilson <ethanwilson1291@gmail.com>
2025-02-07 00:40:02 +0700

    feat: onboard - change default theme to light

--- apps/web-np/src/preferences.ts
+++ apps/web-np/src/preferences.ts
@@ -1,15 +1,12 @@
 import { defineOverridesPreferences } from '@vben/preferences';
 
-/**
- * @description 项目配置文件
- * 只需要覆盖项目中的一部分配置，不需要的配置不用覆盖，会自动使用默认配置
- * !!! 更改配置后请清空缓存，否则可能不生效
- */
 export const overridesPreferences = defineOverridesPreferences({
-  // overrides
   app: {
     name: import.meta.env.VITE_APP_TITLE,
     locale: 'en-US',
+  },
+  theme: {
+    mode: 'light',
   },
   tabbar: {
     enable: false,
